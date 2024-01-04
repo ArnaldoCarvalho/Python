ce318ee2bda58f4152dfac88e9c3112a6612a473
ArnaldoCunhaCarvalho <arcuca0@gmail.com>
2024-01-04 14:11:49 +0000

    Update JogoCapitais.py

--- 003-Trabalho/JogoCapitais.py
+++ 003-Trabalho/JogoCapitais.py
@@ -13,8 +13,84 @@ def main():
     ''' É Utilizado este input para permitir o utilizador ler os dados antes do programa encerrar '''
     input()
 
+def load():
+    try:
+        file = open('./text.txt','r')
+        fileS = file.read().split('|')
+        count = 0
+        for x in fileS:
+            paises.append(fileS[count+1])
+            capitais.append(fileS[count+2])
+            count = count + 3
+        return 1
+    except:
+        print('erro no carregamento do ficheiro')
+        return 0
+    
 def game():
+    if (load()==0):
+        return
     vitoria = 0
+    load()
+
+    for x in range(4):
+        y = random.randint(1,len(paises))
+        pais = paises[y]
+        capital = capitais[y]
+
+        print('Qual a capital da ', pais)
+
+        poss = random.randint(1,4)
+
+        if poss == 1:
+            print('A) ',capital) 
+            print('B) ')
+            print('C) ')
+            print('D) ')
+        if poss == 1:
+            print('A) ') 
+            print('B) ',capital)
+            print('C) ')
+            print('D) ')
+        if poss == 1:
+            print('A) ') 
+            print('B) ')
+            print('C) ',capital)
+            print('D) ')
+        if poss == 1:
+            print('A) ') 
+            print('B) ')
+            print('C) ')
+            print('D) ',capital)
+
+
+        v = 0
+        result = 0
+        while v == 0:
+            resposta = input("Introduza a letra da opção")
+            resposta = resposta.upper()
+
+            if resposta == 'A':
+                result = 1
+                v=1
+            else:
+                if resposta == 'B':
+                    result = 2
+                    v=1
+                else:
+                    if resposta == 'C':
+                        result = 3
+                        v=1
+                    else:
+                        if resposta == 'D':
+                            result = 4
+                            v=1
+                        else:
+                            v=0
+        
+        if result == poss:
+            vitoria = vitoria + 1
+            
     return vitoria
 
 ''' Chamada do main para ver se ele está funcionando '''
